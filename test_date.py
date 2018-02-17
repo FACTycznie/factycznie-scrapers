e980e3fc86a2f84add45188c88d7592323cf2f72
Krzysztof Cybulski <k.cybulski.dev@gmail.com>
2018-02-18 00:37:54 +0100

    Add timestamp testing script

---
+++ test_date.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This scripts allows to easily test if our timestamp retrieval system works
+for a given news website. It simply prints out the output we get, or throws the
+exception it would give.
+"""
+from factscraper import parse
+import sys
+
+if len(sys.argv) < 2:
+    print("""Usage:
+    ./test_date.py <url to article>
+""")
+    exit(0)
+
+print(parse(sys.argv[1])['timestamp'])
