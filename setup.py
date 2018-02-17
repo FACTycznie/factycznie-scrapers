42ef31b9623342859baee1aa5fd24a130de93c0d
Krzysztof Cybulski <k.cybulski.dev@gmail.com>
2018-02-17 19:27:45 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    if sys.version_info[0] == 2:
+        required = f.read().splitlines()
+    else:
+        required = []
+        for package in f.read().splitlines():
+            if 'futures' not in package:
+                required.append(package)
+
+setup(
+    name='factscraper',
+    version='0.5.3',
+    packages=['factscraper'],
+    install_requires=required,
+    license='GNU General Public License v3.0',
+)
+
