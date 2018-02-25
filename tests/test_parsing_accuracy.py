894e63fe3a6a5cd869c96e35468dff30f7e81ff8
Krzysztof Cybulski <k.cybulski.dev@gmail.com>
2018-02-25 17:06:12 +0100

    Add more test articles and make more robust testing set up
    Testing set up has been made more robust by `pass`ing when an exception
    happens during preliminary analysis of links in a loop.

--- tests/test_parsing_accuracy.py
+++ tests/test_parsing_accuracy.py
@@ -107,7 +107,93 @@ test_articles = [
         prezydenta Andrzeja Dudy.""",
         date(year=2018, month=2, day=24),
         ['IAR', 'PAP', 'INTERIA.PL'],
-        [])]
+        []),
+    _TestArticle(
+        "http://nt.interia.pl/raporty/raport-zagadki-przyrody/strona-glowna/news-juz-wiadomo-dlaczego-nietoperze-sa-odporne-na-niebezpieczne-,nId,2549307",
+        "http://nt.interia.pl/raporty/raport-zagadki-przyrody/strona-glowna/news-juz-wiadomo-dlaczego-nietoperze-sa-odporne-na-niebezpieczne-,nId,2549307",
+        "nt.interia.pl",
+        "Już wiadomo, dlaczego nietoperze są odporne na niebezpieczne wirusy",
+        """Pojedyncza mutacja w genie o nazwie Sting może być jednym z powodów,
+        dla których nietoperze są odporne na skutki działania potencjalnie
+        śmiercionośnych wirusów, takich jak Ebola. Chińscy naukowcy wykazali,
+        że gen Sting u nietoperzy indukuje produkcję niższych poziomów
+        interferonu, który sygnalizuje, że organizm jest atakowany przez
+        wirusa. Za duże stężenie interferonu wiąże się z wystąpieniem poważnych
+        objawów obserwowanych w wielu zakażeniach wirusowych. Ale dzięki
+        specjalnej mutacji, nietoperze mogą z tym walczyć. Powszechnie wiadomo
+        bowiem, że nietoperze mogą być nosicielami wielu niebezpiecznych
+        wirusów (Marburg, Ebola, Nipah) i jednocześnie nie cierpieć z tego
+        powodu. - Interesowało nas, dlaczego układ odpornościowy nietoperzy
+        może radzić sobie z tak wieloma śmiercionośnymi wirusami - powiedział
+        prof. Peng Zhou z Instytutu Wirusologii w Wuhan. Zazwyczaj w
+        mechanizmach odpornościowych na infekcje wirusowe biorą udział
+        przeciwciała i limfocyty T. Rozpoznają one wirusy, które poznały w
+        wyniku szczepień ochronnych lub wcześniej przebytych infekcji. Ale
+        wirusowych najeźdców można także zwalczać dzięki odporności wrodzonej.
+        Kiedy wirus infekuje komórkę, pozostawia wiele znaków
+        charakterystycznych, a te są wykrywane przez białka, które włączają
+        produkcję interferonu. Wczesne objawy, które wiążemy z infekcją
+        wirusową - gorączka, bóle mięśniowe i zmęczenie - są spowodowane
+        działaniem interferonu. Interferon uruchamia powstawanie innych
+        cząsteczek, które wykazują bezpośredni efekt antywirusowy. Okazuje się
+        jednak, że za duże ilości interferonu także szkodzą. To właśnie przez
+        ten szczegół niektóre wirusy, takie jak Ebola czy SARS są
+        niebezpieczne. - Początkowo myśleliśmy, że nietoperze mają bardzo silny
+        układ odpornościowy, ale po dokładniejszym badaniu genomu nietoperzy
+        stwierdziliśmy, że wykorzystywany przez nie mechanizm jest wyjątkowy -
+        powiedział prof. Zhou. Szczególnie ważną rolę w walce z wirusami
+        odgrywa gen Sting. Dzięki niemu możliwe jest wykrywanie obcego DNA w
+        miejscu, w którym normalnie nie powinno go być, czyli w cytoplazmie
+        komórki. Niektóre badania sugerują także, że dzięki Sting nietoperze
+        mogą wykrywać RNA - alternatywny materiał genetyczny często
+        wykorzystywany przez wirusy. - Odkryliśmy, że gen Sting wpływa na
+        wytwarzanie interferonu przez nietoperze, co utrudnia
+        rozprzestrzenianie się wirusów - dodał prof. Zhou. Naukowcy nie są
+        jednak pewni, czy stłumienie wytwarzania interferonu przez zmieniony
+        gen Sting, zapewnia bezpośrednią ochronę przed niebezpiecznymi
+        wirusami. Konieczne są dalsze badania do potwierdzenia tych
+        rewelacji.""",
+        date(year=2018, month=2, day=24),
+        ['INTERIA.PL'],
+        []),
+    _TestArticle(
+        "https://wiadomosci.onet.pl/swiat/francja-od-stycznia-udaremniono-dwa-ataki-terrorystyczne/gd12e97",
+        "https://wiadomosci.onet.pl/swiat/francja-od-stycznia-udaremniono-dwa-ataki-terrorystyczne/gd12e97",
+        "wiadomosci.onet.pl",
+        "Francja: od stycznia udaremniono dwa ataki terrorystyczne",
+        """Od stycznia we Francji udaremniono dwa planowane ataki
+        terrorystyczne, w tym jeden na obiekt sportowy, a drugi na siły
+        wojskowe - poinformował w niedzielę francuski minister spraw
+        wewnętrznych Gerard Collomb. W wywiadzie dla radia Europe 1, telewizji
+        C-News i dziennika ekonomicznego "Les Echos" minister powiedział
+        ogólnie, że zamachy te udaremniono "na południu" i "na zachodzie"
+        Francji. - Celem była młodzież - dodał, odnosząc się do planu ataku na
+        - jak to ujął - "duży obiekt sportowy". W wywiadzie dla radia Europe 1,
+        telewizji C-News i dziennika ekonomicznego "Les Echos" minister
+        powiedział ogólnie, że zamachy te udaremniono "na południu" i "na
+        zachodzie" Francji. - Celem była młodzież - dodał, odnosząc się do
+        planu ataku na - jak to ujął - "duży obiekt sportowy". Szef
+        francuskiego MSW powiadomił, że podejrzane osoby zostały zatrzymane i
+        przebywają w areszcie. - W ten sposób udało nam się udaremnić te plany
+        - dodał Collomb, ale nie ujawnił więcej szczegółów na temat
+        aresztowania, ani celów ataków. Wskazał jedynie, że chodzi o osoby
+        widniejące już w tzw. kartotece osób zradykalizowanych (FSPRT), która
+        we Francji prowadzona jest od 2015 roku i ma zasadnicze znaczenie dla
+        walki z terroryzmem. Figurują w niej osoby, które stosują przemoc bądź
+        mogą się do niej uciekać. Są tam dane osobowe, adres, opis sytuacji
+        prawnej, ewentualna opinia psychiatryczna, informacje o kontaktach z
+        innymi radykałami. Minister Collomb poinformował również, że w
+        niedzielę we Francji zostały zamknięte trzy meczety - dwa na
+        południowym wschodzie kraju i jeden w regionie paryskim - "ponieważ
+        głoszono w nich pochwałę terroryzmu". Według oficjalnych danych we
+        Francji w 2017 roku udaremniono dwadzieścia ataków terrorystycznych.""",
+        date(year=2018, month=2, day=25),
+        [], # Actually this should be ["Gerard Collomb"] or ["Europe 1",
+        # "C-News" and "Les Echos"] or even ["Francuskie MSW"] but it's
+        # too difficult to retrieve for now
+        [])
+
+    ]
 
 class TestDownload(unittest.TestCase):
     """This test case checks if `factscraper.downloader` works."""
@@ -123,8 +209,12 @@ class TestDownload(unittest.TestCase):
 
 
 # Download them now so we don't have to redownload them for each test
-_downloaded_articles = [analyze_url(article.initial_url)
-                        for article in test_articles]
+_downloaded_articles = []
+for test_article in test_articles:
+    try:
+        _downloaded_articles.append(analyze_url(test_article.initial_url))
+    except InvalidArticleError:
+        pass
 
 class TestGeneralAnalysis(unittest.TestCase):
     """This test case downloads and analyzes all articles from
