f178f490e9cb334fab88a376073d21f791f663f8
Krzysztof Cybulski <k.cybulski.dev@gmail.com>
2018-02-25 17:23:33 +0100

    Add functionality to analyse scrapy responses directly
    Since analysis will now work on downloaded responses rather than only
    pure urls it will be possible to use it in scrapy spiders.

--- factscraper/__init__.py
+++ factscraper/__init__.py
@@ -19,6 +19,10 @@ from factscraper import parsers, downloader
 def analyze_url(url, verbose=False, parser=None):
     """Returns analysis results of an article with the given url."""
     response = downloader.download(url)
+    return analyze_response(response, verbose=verbose, parser=parser)
+
+def analyze_response(response, verbose=False, parser=None):
+    """Returns analysis results of an article from a response instance."""
     if parser is None:
         parser = parsers.select_parser(response.url)
     if verbose:
