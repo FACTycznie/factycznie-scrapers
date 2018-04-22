62588fc056b92c26cba572fdf41db5b1832c86c1
Krzysztof Cybulski <k.cybulski.dev@gmail.com>
2018-04-22 15:47:11 +0200

    Update parse_url for no authors/sources

--- parse_url.py
+++ parse_url.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 import sys
 
-from factscraper import analyze_url
+from factscraper import analyze_url, InvalidArticleError
 
-to_print = ['url', 'title', 'text', 'publish_date', 
-            'authors', 'sources', 'domain']
+to_print = ['url', 'title', 'text', 'publish_date', 'domain']
 
 if len(sys.argv) == 2:
-    parsed = analyze_url(sys.argv[1], verbose=True)
+    try:
+        parsed = analyze_url(sys.argv[1], verbose=True)
+    except InvalidArticleError:
+        print("InvalidArticleError occured. Parsing without validation")
+        parsed = analyze_url(sys.argv[1], verbose=True, validate=False)
     for key in to_print:
         print("\t{}:\n{}".format(key.capitalize(), parsed[key]))
 else:
